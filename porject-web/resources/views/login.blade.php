cef8f1b8c26a5499a7660f0c607c2c6ffdceaed2
Nelsen Anggara <nelsen12anggara@gmail.com>
2022-10-18 15:58:25 +0700

    coba login page

---
+++ porject-web/resources/views/login.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="./styling/login/login.css">
+    <link rel="stylesheet" href="./styling/default/default.css">
+</head>
+<body>
+    <div id="login">
+        <header>
+            logo
+            <x-css-close />
+        </header>
+        <main>
+            <div id="form-wrapper">
+                <h1>Log in</h1>
+                <form action="">
+                    <div>
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email">
+                    </div>
+                    <div>
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password">
+                    </div>
+                    <button type="submit">Log in</button>
+                    <button>Register</button>
+                </form>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
