2943f2938ed5b4995a755739626c34acce2386ea
Nelsen Anggara <nelsen12anggara@gmail.com>
2022-12-08 12:42:18 +0700

    update login menu

--- LaraStrap/resources/views/component/login-menu.blade.php
+++ LaraStrap/resources/views/component/login-menu.blade.php
@@ -2,21 +2,21 @@
 <div id="menu-wrapper">
     <div id="menu">
         <div id="menu-header">
-            <img src="./Asset/logo.png" width="50px" alt="">
+            <img src="./Asset/Image/logo.png" width="50px" alt="laraStrap Icon">
             <h3>LaraStrap</h3>
         </div>
         <div id="menu-list">
-            <a href="/">
+            <a class="underline" href="/">
                 Home
-                <img src="" alt="RightArrow">
+                <img src="./Asset/Icon/next.png" alt="RightArrow">
             </a>
-            <a href="#">
+            <a class="underline" href="#">
                 Course
-                <img src="" alt="RightArrow">
+                <img src="./Asset/Icon/next.png" alt="RightArrow">
             </a>
             <a href="#">
                 Study List
-                <img src="" alt="RightArrow">
+                <img src="./Asset/Icon/next.png" alt="RightArrow">
             </a>
         </div>
     </div>
@@ -24,10 +24,12 @@
 
 <style>
     #backDrop {
-        z-index: 2;
+        z-index: 1;
         height: 100vh;
         width: 100%;
-        /* display: block | none; */
+        position: absolute;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: none;
     }
 
     #menu-wrapper{
@@ -41,9 +43,18 @@
         font-family: boldp;
     }
 
+    #menu{
+        background-color: white;
+        z-index: 3;
+    }
+
     #menu-header{
         align-items: center;
         display: flex;
+    }
+
+    #menud-header:hover ~ #menu ~ #menu-wrapper #backDrop{
+        display: block !important;
     }
 
     #menu-list{
@@ -53,12 +64,21 @@
     }
 
     #menu-list a{
+        width: 10vw;
         color: #03353C;
         font-family: regularp;
         display: flex;
         justify-content: space-between;
+        margin-top: 1vh;
+        align-items: center;
+    }
+
+    #menu-list a img{
+        width: 25px;
+    }
+
+    .underline{
         border-bottom: 1px solid black;
         padding-bottom: 1vh;
-        margin-top: 1vh;
     }
 </style>
