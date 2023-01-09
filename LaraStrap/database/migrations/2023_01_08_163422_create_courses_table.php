d5290aaa90c16dbd881d58f77fd075fade3b87ce
FelixKD <timotiusfelix2@gmail.com>
2023-01-09 16:07:39 +0700

    ilangin kolom logo

--- LaraStrap/database/migrations/2023_01_08_163422_create_courses_table.php
+++ LaraStrap/database/migrations/2023_01_08_163422_create_courses_table.php
@@ -17,7 +17,6 @@ class CreateCoursesTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('description');
-            $table->string('logo');
             $table->string('video');
             // bisa ditambahin lagi sesuai konten di page course
         });
