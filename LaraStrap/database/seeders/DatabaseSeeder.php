cca2079a1c808cba803a5ec7cdfd7cea953d59dd
FelixKD <timotiusfelix2@gmail.com>
2023-01-09 15:54:22 +0700

    Update DatabaseSeeder.php

--- LaraStrap/database/seeders/DatabaseSeeder.php
+++ LaraStrap/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,69 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('courses')->insert([
+            'name' => 'HTML Elements',
+            'description' => 'An HTML element is defined by a start tag, some content, and an end tag.',
+            'logo' => 'a',
+            'video' => 'PATH'
+        ]);
+
+        DB::table('courses')->insert([
+            'name' => 'HTML Attributes',
+            'description' => 'HTML attributes provide additional information about HTML elements.',
+            'logo' => 'a',
+            'video' => 'PATH'
+        ]);
+
+        DB::table('courses')->insert([
+            'name' => 'HTML Forms',
+            'description' => 'An HTML form is used to collect user input. The user input is most often sent to a server for processing.',
+            'logo' => 'a',
+            'video' => 'PATH'
+        ]);
+
+        DB::table('courses')->insert([
+            'name' => 'CSS Syntax',
+            'description' => 'A CSS rule consists of a selector and a declaration block.',
+            'logo' => 'a',
+            'video' => 'PATH'
+        ]);
+
+        DB::table('courses')->insert([
+            'name' => 'CSS Selector',
+            'description' => 'A CSS selector selects the HTML element(s) you want to style.',
+            'logo' => 'a',
+            'video' => 'PATH'
+        ]);
+
+        DB::table('courses')->insert([
+            'name' => 'CSS Pseudo-class',
+            'description' => 'A pseudo-class is used to define a special state of an element.',
+            'logo' => 'a',
+            'video' => 'PATH'
+        ]);
+
+        DB::table('courses')->insert([
+            'name' => 'JS Output',
+            'description' => 'JavaScript can "display" data in different ways.',
+            'logo' => 'a',
+            'video' => 'PATH'
+        ]);
+
+        DB::table('courses')->insert([
+            'name' => 'JS Syntax',
+            'description' => 'JavaScript syntax is the set of rules.',
+            'logo' => 'a',
+            'video' => 'PATH'
+        ]);
+
+        DB::table('courses')->insert([
+            'name' => 'JS Data Types',
+            'description' => 'JavaScript has 8 datatypes and an object datatype.',
+            'logo' => 'a',
+            'video' => 'PATH'
+        ]);
+
+
     }
 }
